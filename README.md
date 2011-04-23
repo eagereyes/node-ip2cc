119e2e13651adf3c9ea3ddc449e17f048db2eac4
Robert Kosara <rkosara@me.com>
2011-04-23 10:18:10 -0400

    Queue lookUp() calls if data not loaded, load data if not loaded, add listen() function that creates a simple server, update README with some documentation.

--- README.md
+++ README.md
@@ -2,7 +2,38 @@
 
 This program provides the means to find the country an IP address is likely used in. The program reads in a publicly available file with definitions, and performs a binary search on them. Adjacent blocks belonging to the same country are joined, which reduces the number of items by about 50%. The binary search has to perform a maximum of 16 steps, which makes lookups extremely fast.
 
-Turning this into a server should be simple, and since the data is kept in memory, it should be able to handle large numbers of requests. 
+Loading the data into memory takes a few seconds, but that only has to be done once. When running as an HTTP server, requests take less than 0.3ms on a 2.5GHz MacBook Pro.
+
+## Usage
+
+Quick example:
+
+```javascript
+lookUp('174.96.192.67', function(ipaddress, country) {
+	if (country) {
+		console.log(ipaddress+': '+country);
+	} else {
+		console.log(ipaddress+' not found');
+	}
+});
+```
+
+### Function lookUp(ipaddress, callback)
+
+This function performs the lookup and calls the callback, which gets two parameters: `callback(ip, cc)`, where ip is the ipaddress the way it was passed to the function, and cc is the two-letter country code if one was found, or null if not.
+
+On the first call, `lookUp()` will initiate the IP data to be loaded from disk. Calls to `lookUp()` while the data is loading are queued, and are performed in the order in which they were received.
+
+### Function listen(port)
+
+Creates a simple server process that listens on `port` for requests and returns a simple JSON object as the response. The IP to be looked up is passed in the URL. If the server is listening on port 4711, a request to http://127.0.0.1:4711/174.96.192.67 will result in the following response:
+
+```javascript
+{
+	"ip":"174.96.192.67",
+	"cc":"US"
+}
+```
 
 ## Dependencies
 
@@ -16,7 +47,7 @@ The included datafile is licensed under the [GNU General Public License, version
 
 The code is licensed under the [ISC License](http://www.opensource.org/licenses/isc-license), which is practically identical to the better-known MIT License, but uses simpler wording (see below).
 
-Copyright (c) 2011, Robert Kosara <rkosara@me.com>
+Copyright (c) 2011, Robert Kosara (rkosara@me.com)
 
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
--- ip2country.js
+++ ip2country.js
@@ -1,41 +1,35 @@
+//
+// ip2country.js
+//
+// Copyright (c) 2011, Robert Kosara <rkosara@me.com>
+// 
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 var csv = require('csv');
+var http = require('http');
 
 var FILENAME = 'IpToCountry.csv';
 
 var ipList = [];
 
-function lookUp(ipaddress, callback) {
-	var address;
-	if (typeof(ipaddress) === 'number') {
-		address = ipaddress;
-	} else {
-		if (ipaddress.indexOf('.') >= 0) {
-			var bytes = ipaddress.split('.');
-			address = parseInt(bytes[0], 10)*16777216 + parseInt(bytes[1], 10)*65536 +
-						parseInt(bytes[2], 10)*256 + parseInt(bytes[3], 10);
-		} else {
-			address = parseInt(ipaddress, 10);
-		}
-	}
-	var bottom = 0;
-	var top = ipList.length-1;
-	var mid = 0;
-	do {
-		mid = Math.floor((bottom+top)/2);
-		if (address > ipList[mid].end) {
-			bottom = mid + 1;
-		} else {
-			top = mid - 1;
-		}
-	} while (!(address >= ipList[mid].start && address <= ipList[mid].end) && (top >= bottom));
-	if (address >= ipList[mid].start && address <= ipList[mid].end) {
-		callback(ipaddress, ipList[mid].cc);
-	} else {
-		callback(ipaddress, null);
-	}
-}
+var initialized = false;
 
-function loadData(callback) {
+var loading = false;
+
+var callbacks = [];
+
+function loadData() {
+	loading = true;
 	csv()
 	.fromPath(FILENAME).transform(function(data) {
 		if (data[0][0] != '#') {
@@ -45,26 +39,93 @@ function loadData(callback) {
 		}
 	})
 	.on('data', function(data, index) {
-		var entry = {
-			start: 	parseInt(data[0], 10),
-			end: 	parseInt(data[1], 10),
-			cc: 	data[4]
-			};
-		var previous = ipList[ipList.length-1];
-		// if (ipList.length > 0 && entry.start-previous.end > 1) {
-		// 	console.log('gap between '+previous.end+' and '+entry.start);
-		// }
-		if (ipList.length > 0 && entry.start == previous.end+1 && previous.cc == entry.cc) {
-			previous.end = entry.end;
-		} else {
-			ipList.push(entry);
+		if (data[4] != 'ZZ') {
+			var entry = {
+				start: 	parseInt(data[0], 10),
+				end: 	parseInt(data[1], 10),
+				cc: 	data[4]
+				};
+			var previous = ipList[ipList.length-1];
+			if (ipList.length > 0 && entry.start == previous.end+1 && previous.cc == entry.cc) {
+				previous.end = entry.end;
+			} else {
+				ipList.push(entry);
+			}
 		}
 	})
 	.on('end', function(count) {
-		callback();
+		initialized = true;
+		callbacks.forEach(function(callback) {
+			callback();
+		});
 	})
 	.on('error', function(error){
 		console.error(error.message);
+	});
+}
+
+function ready(callback) {
+	if (initialized) {
+		callback();
+	} else {
+		callbacks.push(callback);
+		if (!loading) {
+			loadData();
+		}
+	}
+}
+
+function lookUp(ipaddress, callback) {
+	if (!initialized) {
+		ready(function() {
+			lookUp(ipaddress, callback);
+		});
+	} else {
+		var address;
+		if (typeof(ipaddress) === 'number') {
+			address = ipaddress;
+		} else {
+			if (ipaddress.indexOf('.') >= 0) {
+				var bytes = ipaddress.split('.');
+				address = parseInt(bytes[0], 10)*16777216 + parseInt(bytes[1], 10)*65536 +
+							parseInt(bytes[2], 10)*256 + parseInt(bytes[3], 10);
+			} else {
+				address = parseInt(ipaddress, 10);
+			}
+		}
+		var bottom = 0;
+		var top = ipList.length-1;
+		var mid = 0;
+		do {
+			mid = Math.floor((bottom+top)/2);
+			if (address > ipList[mid].end) {
+				bottom = mid + 1;
+			} else {
+				top = mid - 1;
+			}
+		} while (!(address >= ipList[mid].start && address <= ipList[mid].end) && (top >= bottom));
+		if (address >= ipList[mid].start && address <= ipList[mid].end) {
+			callback(ipaddress, ipList[mid].cc);
+		} else {
+			callback(ipaddress, null);
+		}
+	}
+}
+
+function listen(port) {
+	if (!loading) {
+		loadData();
+	}
+	ready(function() {
+		http.createServer(function (req, res) {
+			lookUp(req.url.substring(1), function(ip, cc) {
+				res.writeHead(200, {'Content-Type': 'application/json'});
+				res.end(JSON.stringify({
+					ip: ip,
+					cc: cc
+				}));
+			});
+		}).listen(port, '127.0.0.1');
 	});
 }
 
@@ -76,9 +137,29 @@ function testOut(ipaddress, country) {
 	}
 }
 
-loadData(function() {
-	lookUp('174.96.190.61', testOut);
-	lookUp('3640291329', testOut);
-	lookUp(460598268, testOut);
-});
+function test() {
+	lookUp('127.0.0.1', testOut);
 
+	lookUp('174.96.192.67', function(ipaddress, country) {
+		if (country) {
+			console.log(ipaddress+': '+country);
+		} else {
+			console.log(ipaddress+' not found');
+		}
+	});
+
+	ready(function () {
+		lookUp('174.96.190.61', testOut);
+		lookUp('0.0.17.45', testOut);
+	});
+
+	ready(function() {
+		lookUp('3640291329', testOut);
+	});
+
+	ready(function() {
+		lookUp(460598268, testOut);
+	});
+
+	listen(4711);
+}
